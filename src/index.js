1d5eba01b568192360ec650da987d96525e12c2d
Serthii Diadenchuk <dyadenchuks@gmail.com>
2018-11-23 16:19:43 +0200

    Initial App structure. Add basic React components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-/* eslint-disable no-console */
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-console.log("Hello World!");
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
